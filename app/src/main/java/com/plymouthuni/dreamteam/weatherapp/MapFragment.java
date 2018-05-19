d18183920dbb6744c24168bd5198ba73e463f534
tommaddoxsoftware <32769550+tommaddoxsoftware@users.noreply.github.com>
2018-05-19 19:49:33 +0100

    Created checks for default fragment
    Checks if user is logged in via savedstate, loads default fragment (Set to login at the moment)

--- app/src/main/java/com/plymouthuni/dreamteam/weatherapp/MapFragment.java
+++ app/src/main/java/com/plymouthuni/dreamteam/weatherapp/MapFragment.java
@@ -9,6 +9,7 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 import com.google.android.gms.maps.GoogleMap;
@@ -55,7 +56,7 @@ public class MapFragment extends Fragment {
                     google_map.getUiSettings().setMyLocationButtonEnabled(true);
                 } else {
 
-                    //Toast.makeText(this, R.string.error_permission_map, Toast.LENGTH_LONG).show();
+                    Toast.makeText(getContext(), R.string.error_permission_map, Toast.LENGTH_LONG).show();
                 }
 
                 // Centre on users current position
